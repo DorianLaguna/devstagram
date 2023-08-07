7dd1862f8c85c8c68fbf9d9c4529c727e0a80ee9
DorianLaguna <richardlgcs@gmail.com>
2023-08-07 16:54:16 -0600

    Likes with sumassion in each post asocciate with users

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -39,3 +41,13 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.
 Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
 
 Route::post('/imagenes',[ImagenController::class, 'store'])->name('imagenes.store');
+
+
+//like a las fotos
+Route::post('post/{post}/Likes', [LikeController::class, 'store'])->name('posts.likes.store');
+
+Route::delete('post/{post}/Likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+//Rutas para perfil
+Route::get('{{user:username}}/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('{{user:username}}/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
