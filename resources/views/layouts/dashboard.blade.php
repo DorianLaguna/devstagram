f0ebe2c633e5807f324a30597a6984916f1cc8a2
DorianLaguna <richardlgcs@gmail.com>
2023-08-17 12:43:23 -0600

    Ultimo cambio de boton conocer gente

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -76,15 +76,17 @@
         </div>
     </div>
 
-    <div class="flex justify-center w-35 mx-auto mt-2">
-        <a href="{{route('conocer')}}" 
-            class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded sm:text-sm text-xs uppercase font-bold cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                </svg>                                           
-            Conocer gente
-        </a>
-    </div>
+    @if ($user->id === auth()->user()->id)
+        <div class="flex justify-center w-35 mx-auto mt-2">
+            <a href="{{route('conocer')}}" 
+                class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded sm:text-sm text-xs uppercase font-bold cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                    </svg>                                           
+                Conocer gente
+            </a>
+        </div>        
+    @endif
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-3">Publicaciones</h2>
